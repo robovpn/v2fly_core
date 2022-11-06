deef4c6253c403dc2b8f8afc4b777e5b0e1070f5
kispravnikov <113772276+kispravnikov@users.noreply.github.com>
2022-11-06 13:00:29 +0300

    Delete proxy directory

--- proxy/http/http.go
+++
@@ -1,3 +0,0 @@
-package http
-
-//go:generate go run /v2fly_core/common/errors/errorgen
--- proxy/vless/vless.go
+++
@@ -1,8 +0,0 @@
-// Package vless contains the implementation of VLess protocol and transportation.
-//
-// VLess contains both inbound and outbound connections. VLess inbound is usually used on servers
-// together with 'freedom' to talk to final destination, while VLess outbound is usually used on
-// clients with 'socks' for proxying.
-package vless
-
-//go:generate go run /v2fly_core/common/errors/errorgen
